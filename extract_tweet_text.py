fc7d82ddaabbbc3dc7590a97328dd6a55854ab00
E.K. Sanders <emily.kathleen.sanders+git@gmail.com>
2024-10-18 18:26:41 -0400

    beginning script

---
+++ extract_tweet_text.py
@@ -0,0 +1,13 @@
+# Imports, part 1
+import pandas as pd
+import requests
+import getpass
+from datetime import date, time, datetime
+import time
+import os
+
+# Imports, part 2: change wd, get my functions, change it back
+os.chdir('../../')
+from emilys_functions import my_date, autoplots
+os.chdir('./projects/twitter_practice/')
+
